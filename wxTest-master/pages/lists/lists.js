c0a379e027575185e329f9532ddb5859bc805be8
xueandong <48705753+xueandong@users.noreply.github.com>
2019-04-16 13:09:56 +0800

    Add files via upload

--- wxTest-master/pages/lists/lists.js
+++ wxTest-master/pages/lists/lists.js
@@ -1,8 +1,40 @@
 Page({
-  formSubmit(e) {
-    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  data:{
+    benjin:'',
+    lixi:'',
+    mon:'',
+    still:[],
+    still1:[],
+
   },
-  formReset() {
-    console.log('form发生了reset事件')
+  userBenjinInput:function(e){
+    this.setData({
+      benjin:e.detail.value
+    })
+  },
+  userLixiInput:function(e){
+    this.setData({
+      lixi:e.detail.value
+    })
+  },
+  userMon:function(e){
+    this.setData({
+      mon:e.detail.value,
+    })
+  },
+  jisuan:function(e){
+    var benjin = this.data.benjin;
+    var lixi = this.data.lixi;
+    var mon = this.data.mon;
+    var still1 = new  Array();
+    var still = new Array();
+    var still = this.data.still;
+    for (var i = 0; i <mon; i++) {
+    benjin=(benjin * (lixi / 100)) + benjin * 1;
+    still1[i]= benjin;
+    }
+    this.setData({
+      still:still1
+    })
   }
 })
