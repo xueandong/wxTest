59abe4ffbc4aa76ace35e6c14edaa7bf9983397a
xueandong <48705753+xueandong@users.noreply.github.com>
2019-05-07 11:37:21 +0800

    Add files via upload

---
+++ wxtest/components/Topmove/Topmove.js
@@ -0,0 +1,43 @@
+// components/Topmove/Topmove.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    TopMove:[{
+
+      name:"好"
+    },
+      {
+        name: "优秀"
+      },
+      {
+        name: "很优秀"
+      },
+      {
+        name: "非常优秀"
+      },
+      {
+        name: "很好"
+      },
+      {
+        name: "非常好"
+      },
+      
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
